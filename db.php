2fe160038a90741732946c617e7110767347f150
Manoy Hardeveld <2176730@talnet.nl>
2023-12-05 14:13:35 +0100

    insert en select update

--- db.php
+++ db.php
@@ -1,18 +1,23 @@
 <?php
 class Database {
     public $pdo;
+    private $table = "user";
 
     public function __construct($db = "test", $host = "localhost:3307", $user = "root", $pwd = "") {
-        try {
-            $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
-            // set the PDO error mode to exception
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected to database $db";
-        } catch (PDOException $e) {     
-            echo "Connection failed: " . $e->getMessage();
-        }
+        $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+        // set the PDO error mode to exception
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function insert($email, $password) {
+        $stmt = $this->pdo->prepare("INSERT INTO $this->table (email, password) VALUES (?, ?)");
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $stmt->execute([$email, $password]);
+    }
+
+    public function selectAll() {
+        $stmt = $this->pdo->query("SELECT * FROM $this->table");
+        return $stmt;
     }
 }
-
-$pdo = new Database();
 ?>
--- home.php
+++ home.php
@@ -1,4 +1,54 @@
-<?php 
-include 'db.php';
+<?php
+    include 'db.php';
 
-?>
+    try {
+        $db = new Database();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $db->insert($_POST['email'], $_POST['password']);
+            echo "Successfully submitted";
+        }
+    } catch (\Exception $e){
+        echo "ERROR: " . $e->getMessage();
+    }
+    ?>
+
+    <table class="table">
+        <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Password</th>
+        </tr>
+
+        <?php
+        $stmt = $db->selectAll();
+        if ($stmt->rowCount() > 0) {
+            while ($row = $stmt->fetch()) {
+                echo "<tr>";
+                echo "<td>" . $row["id"] . " | ";
+                echo "<td>" . $row['email'] . " | ";
+                echo "<td>" . $row['password'] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='3'>No users found</td></tr>";
+        }
+        ?>
+    </table>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+        <input type="text" name="email">
+        <input type="password" name="password">
+        <input type="submit">
+    </form>
+
+
+</body>
+</html>
